ac13fce2b11ff3968c5d0a98fe73688a4dfa35fa
Colin But <colinbut@users.noreply.github.com>
2018-04-19 15:34:31 +0100

    Moving old project files across

---
+++ src/main/java/com/mycompany/birthday/birthdayapp/BirthdayAppApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.birthday.birthdayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mycompany/birthday/birthdayapp/BirthdayAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.birthday.birthdayapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BirthdayAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
